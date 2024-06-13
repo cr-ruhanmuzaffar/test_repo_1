62900b072574f6012d1c83b757f25fb8ac2b0a82
cr-ruhanmuzaffar <ruhan@coderabbit.ai>
2024-06-13 18:14:04 +0530

    Create sampleGo.go
    test golangCli tool

---
+++ sampleGo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// The function name should be in camelCase according to Go naming conventions
+func Hello_world() {
+    // Unused variable 'msg'
+    msg := "Hello, World!"
+    fmt.Println("Hello, World!")
+}
+
+func main() {
+    Hello_world()
+}
